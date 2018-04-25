010a74abcdb4bc23212d7d7292f7c693c19a5ef1
Reid Evans <reidev275@gmail.com>
2018-04-25 10:33:33 -0400

    generic filter

--- src/filters.ts
+++ src/filters.ts
@@ -1,15 +1,20 @@
 import { HttpContext } from "./web";
 import { WebPart, pure, fail } from "./webPart";
+import { Fn } from "./prelude/common";
 
-export const method = (method: string): WebPart<HttpContext> => ({
-  run: ctx => (ctx.req.method === method ? pure(ctx) : fail<HttpContext>())
+export const filter = (
+  pred: Fn<HttpContext, boolean>
+): WebPart<HttpContext> => ({
+  run: ctx => (pred(ctx) ? pure(ctx) : fail<HttpContext>())
 });
+
+export const method = (method: string): WebPart<HttpContext> =>
+  filter(x => x.req.method === method);
 
 export const GET = method("GET");
 export const PUT = method("PUT");
 export const POST = method("POST");
 export const DELETE = method("DELETE");
 
-export const path = (path: string): WebPart<HttpContext> => ({
-  run: ctx => (ctx.req.path === path ? pure(ctx) : fail<HttpContext>())
-});
+export const path = (path: string): WebPart<HttpContext> =>
+  filter(x => x.req.path === path);
